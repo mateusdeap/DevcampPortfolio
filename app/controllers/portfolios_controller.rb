52eb4ec601223b8e06b696d666f5bc77fdfd671d
Mateus Pereira <mateusdeap@gmail.com>
2019-07-18 10:43:01 -0300

    Lesson 63

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,9 +16,9 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html {
+        format.html do
           redirect_to portfolios_path, notice: 'Your portfolio item is now live'
-        }
+        end
       else
         format.html { render :new }
       end
@@ -34,11 +34,21 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.update(portfolio_item_params)
-        format.html {
+        format.html do
           redirect_to portfolios_path, notice: 'Record successfully updated.'
-        }
+        end
       else
         format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html do
+        redirect_to portfolios_path, notice: 'Portfolio Item removed.'
       end
     end
   end
