ed289fd46b42604dbd107fc90cfa54f5f742e47d
Mateus Pereira <mateusdeap@gmail.com>
2019-08-13 09:14:26 -0300

    Lesson 79

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,11 @@ class Portfolio < ApplicationRecord
   scope :angular_items, -> { where(subtitle: 'Angular') }
 
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'https://via.placeholder.com/600x400'
+    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+  end
 end
