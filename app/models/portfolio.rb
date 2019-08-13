133caf0a7e7dd9aa6675f531f3607d8aaea2af9f
Mateus Pereira <mateusdeap@gmail.com>
2019-08-13 09:39:03 -0300

    Lesson 80

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
   scope :ruby_on_rails_items, -> { where(subtitle: 'Ruby on Rails') }
   scope :angular_items, -> { where(subtitle: 'Angular') }
 
@@ -7,7 +9,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= 'https://via.placeholder.com/600x400'
-    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
   end
 end
