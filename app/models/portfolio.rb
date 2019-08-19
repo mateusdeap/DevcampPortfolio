f279eb47ff183f5bfd1bcbaed3ea58fdc476deac
Mateus Pereira <mateusdeap@gmail.com>
2019-08-19 15:31:33 -0300

    Lesson 82

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,6 +5,8 @@ class Portfolio < ApplicationRecord
   scope :angular_items, -> { where(subtitle: 'Angular') }
 
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: ->(attrs) { attrs['name'].blank? }
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
