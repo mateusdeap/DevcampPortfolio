044c84a089ff47e226c77482ec8d56b5c3cd318d
Mateus Pereira <mateusdeap@gmail.com>
2019-08-19 16:50:31 -0300

    Lesson 83

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,6 +9,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def show
@@ -60,6 +61,7 @@ class PortfoliosController < ApplicationController
   private
 
     def portfolio_item_params
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      params.require(:portfolio).permit(:title, :subtitle, :body,
+                                        technologies_attributes: [:name])
     end
 end
